22df25f898b8812d317f26d566131c48da7b8157
seokjae <seok9211@naver.com>
2021-02-13 17:40:16 +0900

    add testing URI in postman

--- demo_mail/src/main/java/com/example/demo/registration/RegistrationController.java
+++ demo_mail/src/main/java/com/example/demo/registration/RegistrationController.java
@@ -9,11 +9,21 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController {
     private final RegistrationService registrationService;
 
+	// localhost:8081/api/v1/registration
+	/*
+	{
+		"firstName": "name1",
+		"lastName": "name2",
+		"email": "aa@gmail.com",
+		"password": "password"
+	}
+	*/
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 
+	// localhost:8081/api/v1/registration/confirm?token=APP_USER_TOKEN
     @GetMapping(path = "confirm")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
