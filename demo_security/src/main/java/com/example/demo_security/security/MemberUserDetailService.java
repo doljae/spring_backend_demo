72881e7260a266183f9d887a37a275229b4b0766
doljae <seok9211@naver.com>
2021-04-06 20:32:32 +0900

    Update MemberUserDetailService & Impl

--- demo_security/src/main/java/com/example/demo_security/security/MemberUserDetailService.java
+++ demo_security/src/main/java/com/example/demo_security/security/MemberUserDetailService.java
@@ -4,7 +4,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public interface MemberUserDetailService extends UserDetailsService {
+
     @Override
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
--- demo_security/src/main/java/com/example/demo_security/security/MemberUserDetailServiceImpl.java
+++ demo_security/src/main/java/com/example/demo_security/security/MemberUserDetailServiceImpl.java
@@ -5,17 +5,48 @@ import com.example.demo_security.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MemberUserDetailServiceImpl implements MemberUserDetailService {
 
     @Autowired
     private MemberRepository memberRepository;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) {
+        return selectMemberUserDetailByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found!!", username)));
+    }
+
+    public Optional<MemberUserDetail> selectMemberUserDetailByUsername(String username) {
+        return getMemberUserDetails(username)
+                .stream()
+                .filter(memberUserDetail -> username.equals(memberUserDetail.getUsername()))
+                .findFirst();
+    }
+
+
+    public List<MemberUserDetail> getMemberUserDetails(String username) {
+        List<Member> members = memberRepository.findByUsername(username);
+        List<MemberUserDetail> memberUserDetails = new ArrayList<>();
+        for (Member member : members) {
+            MemberUserDetail memberUserDetail = new MemberUserDetail(
+                    member.getUsername(),
+                    passwordEncoder.encode(member.getPassword()),
+                    null,
+                    true,
+                    true,
+                    true,
+                    true
+            );
+            memberUserDetails.add(memberUserDetail);
+        }
+        return memberUserDetails;
     }
 }
