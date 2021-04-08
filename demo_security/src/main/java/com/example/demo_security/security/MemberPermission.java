1da708caff7dc8d5ec5a4aef7d6c88f27a16ac28
doljae <seok9211@naver.com>
2021-04-08 10:59:37 +0900

    add MemberRole.java

--- demo_security/src/main/java/com/example/demo_security/security/MemberPermission.java
+++ demo_security/src/main/java/com/example/demo_security/security/MemberPermission.java
@@ -3,14 +3,12 @@ package com.example.demo_security.security;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 
 @AllArgsConstructor
-@Getter
 public enum MemberPermission {
     READ("user:read"),
     WRITE("user:write");
 
+    @Getter
     private final String permission;
 }
--- demo_security/src/main/java/com/example/demo_security/security/MemberRole.java
+++ demo_security/src/main/java/com/example/demo_security/security/MemberRole.java
@@ -1,5 +1,31 @@
 package com.example.demo_security.security;
 
+import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import static com.example.demo_security.security.MemberPermission.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 public enum MemberRole {
+    USER(Sets.newHashSet(READ)),
+    ADMIN(Sets.newHashSet(READ, WRITE));
+
+    @Getter
+    private final Set<MemberPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
 
 }
